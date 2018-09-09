9d0d11d89adccb8a4c5550fe1f5e9455b35e8bef
Ecconia <github-commit@ecconia.com>
2018-09-10 01:27:07 +0200

    Adding Login event
    Will be the base for data-input later on.

--- src/main/java/de/ecconia/bukkit/plugin/playercache/PlayerCachePlugin.java
+++ src/main/java/de/ecconia/bukkit/plugin/playercache/PlayerCachePlugin.java
@@ -1,8 +1,12 @@
 package de.ecconia.bukkit.plugin.playercache;
 
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class PlayerCachePlugin extends JavaPlugin
+public class PlayerCachePlugin extends JavaPlugin implements Listener
 {
 	@Override
 	public void onLoad()
@@ -11,5 +15,17 @@ public class PlayerCachePlugin extends JavaPlugin
 		//TODO: Load data from DB.
 		//TODO: Provide API.
 		//On any error, do not provide, ¿and API and save to file?
+	}
+	
+	@Override
+	public void onEnable()
+	{
+		getServer().getPluginManager().registerEvents(this, this);
+	}
+	
+	@EventHandler(priority=EventPriority.MONITOR)
+	public void onPreJoin(PlayerLoginEvent event)
+	{
+		getLogger().warning("Login: " + event.getPlayer().getUniqueId() + " - " + event.getPlayer().getName());
 	}
 }
